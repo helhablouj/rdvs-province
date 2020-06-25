f7cb71b6d9137977fa8263fd8e1213de02bea3b1
helhablouj <hamza.elhablouj@gmail.com>
2020-06-25 14:02:45 +0100

    added daam template to test things out

---
+++ resources/views/public/index.blade.php
@@ -0,0 +1,207 @@
+<!DOCTYPE html>
+<!-- saved from url=(0028)https://www.tadamoncovid.ma/ -->
+<html lang="ar"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="description" content="موقع عمالة تارودانت">
+    <meta name="author" content="وزارة الداخلية">
+    <title>موقع عمالة تارودانت</title>
+
+    <!-- Bootstrap core CSS -->
+
+    <link href="{{ asset('daam-template/bootstrap.rtl.min.css')}}" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="{{ asset('daam-template/bootstrap.theme.css')}}" rel="stylesheet">
+    <link href="{{ asset('daam-template/component.css')}}" rel="stylesheet">
+    <link href="{{ asset('daam-template/medias.css')}}" rel="stylesheet">
+    <link href="{{ asset('daam-template/font-awesome.min.css')}}" rel="stylesheet">
+    <link href="{{ asset('daam-template/font.css')}}" rel="stylesheet">
+    
+    <!-- Styles -->
+    <link href="{{ asset('daam-template/styles.css')}}" rel="stylesheet">
+    <script type="text/javascript" src="{{ asset('daam-template/jquery-3.5.1.min.js.téléchargement')}}"></script>
+    <script src="{{ asset('daam-template/bootstrap.min.js.téléchargement')}}" type="text/javascript"></script>
+ 
+    <script type="text/javascript">
+        // window.history.forward();
+        // function noBack() { window.history.forward(); }
+    </script>
+
+    <script type="text/javascript">
+        // history.pushState(null, null, location.href);
+        // window.onpopstate = function () {
+        //     history.go(1);
+        // };
+    </script>
+    <script>
+        .maghribi {
+            font-family: 'Mossad' !important;
+        }
+    </script>
+
+</head>
+<body dir="rtl">
+    <div class="" id="browser-error" style="display: none">
+        <!-- <h1><i class="fa fa-warning"></i> متصفح الإنترنت الخاص بك غير مُحدّث.</h1>
+        <div id="details"> قم بتحديث المتصفح الخاص بك لمزيد من الأمان والسرعة ولأفضل تجربة على هذا الموقع. </div> -->
+    </div>
+    <header>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3 col-6">
+                    <img class="Armoiries" src="{{ asset('daam-template/emblem-maroc.png')}}" alt="Armoiries">
+                    <h1 class="mb-0">المملكة المغربية</h1>
+                    <h1>وزارة الداخلية - عمالة إقليم تارودانت</h1>
+                </div>
+                <!-- <div class="col-md-3 col-6 mobile"><img class="Carte" src="{{ asset('daam-template/carte.svg')}}" width="120" alt=""></div> -->
+                <div class="col-md-6">
+                    <h2>
+                        عمالة إقليم تارودانت - موقع حجز المواعيد الادارية للمرتفقين<br>
+                    </h2>
+                </div>
+                <!-- <div class="col-md-3 desktop">
+                    <img class="Carte" src="{{ asset('daam-template/placeholder.png')}}" alt="">
+                    <h1>عمالة إقليم تارودانت</h1>
+                </div> -->
+            </div>
+        </div>
+    </header>
+
+<div class="marquee">
+    <div>
+        <!-- <p>
+            نحيطكم علما أن خدمة إيداع شكاية عدم التوصل بالدعم قد انتهت &nbsp; <b>يوم الإثنين 25 ماي 2020 عند منتصف الليل</b>،  لتنطلق بعدها عملية المعالجة.
+        </p> -->
+    </div>
+</div>
+<main role="main" class="pt-4 pb-4">
+    <div class="container container-fluid text-rtl">
+        <div class="row mb-5">
+            <div class="col-md-12 text-dark">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="card">
+                            <h3 class="text-teal font-familiy-arab bg-title-red pr-4">حجز موعد باحد اقسام او مصالح عمالة تارودانت</h3>
+                            <div class="card-body">
+                                <div class="event">
+                                    <div class="content">
+                                        <div class="row">
+                                            <div class="col-md-12">
+                                                <div class="text-center" style=" max-width: 700px; margin: 18px auto">
+                                                    <p style="font-size: 22px; font-weight: 600;">
+                                                        يقدم هذا الركن خدمة حجز موعد بأحد اقسام او مصالح عمالة اقليم تارودانت، وذلك في إطار الجهود المبذولة من أجل الحد من انتشار وباء كورونا-كوفيد 19 مع الحرص على استمرارية المرافق العمومية في تقديم خدماتها للمرتفقين مع العمل على الحفاظ على صحة وسلامة الموظفين والمرتفقين.
+                                                    </p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                    <form action="" class="form-horizontal" data-ajax="true" data-ajax-begin="onBeginSuiviDemande" data-ajax-complete="completeAjaxRequete(&#39;#btnSendDemand&#39;, &#39;#imgLoaderbtnSendDemand&#39;)" data-ajax-failure="completeAjaxRequete(&#39;#btnSendDemand&#39;, &#39;#imgLoaderbtnSendDemand&#39;)" data-ajax-method="POST" data-ajax-success="successSendDemand" enctype="multipart/form-data" id="frmSendDemand" method="post" novalidate="novalidate"><input name="__RequestVerificationToken" type="hidden" value="PnAu3RzYD60YE5ZWoynHaCuQOC_7TF2I5idPHyECqg3Tn6D_XlDfhdzG_l4CHi5GujzcL1xKDMyKCPTz3D4sgdlfjEh4WU7X5TE3HaXvG4w1">                                        <fieldset class="Formulaire">
+                                <legend>المرجو إدخال معلومات صاحب طلب الاستفادة من  عملية الدعم</legend>
+
+                                            <div class="row">
+                                                <div class="form-group col-md-6 col-sm-6 col-xs-12">
+                                                    <label class="col-sm-12 col-form-label">رقم البطاقة الوطنية للتعريف الإلكترونية <text style="color:red;">*</text></label>
+                                                    <div class="col-sm-12">
+                                                        <input name="Cnie" autocomplete="off" class="form-control " type="text" value="">
+                                                        <span name="Cnie" class="field-validation-valid" data-valmsg-for="Cnie" data-valmsg-replace="true"></span>
+                                                    </div>
+                                                </div>
+                                                <div class="form-group col-md-6 col-sm-6 col-xs-12">
+                                                    <label class="col-sm-12 col-form-label">رقم الهاتف <text style="color:red;">*</text></label>
+                                                    <div class="col-sm-12">
+                                                        <input name="Telephone" autocomplete="off" class="form-control tooltipstered" data-toggle="tooltip" data-val="true" data-val-required="خانة إجبارية" dir="ltr" id="Telephone" maxlength="10" onblur="checkTel(this);" oncopy="return false" onkeydown="return ValidateTel(this);" onkeyup="return GetMyKeyCode(this,9);" onpaste="return false" type="text" value="">
+                                                        <span name="Telephone" class="field-validation-valid" data-valmsg-for="Telephone" data-valmsg-replace="true"></span>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="row">
+                                                <div class="form-group col-md-6 col-sm-6 col-12">
+                                                    <label class="col-sm-12 col-form-label" onclick="refreshCaptcha(&#39;imgCaptcha&#39;);">كلمة التحقق (إضغط   من أجل تحديث الصورة)</label>
+                                                    <div class="col-sm-9">
+                                                        <img id="imgCaptcha" src="./daam-template/GetCaptchaImage" onclick="refreshCaptcha(&#39;imgCaptcha&#39;);">
+                                                    </div>
+                                                </div>
+
+                                                <div class="form-group col-md-6 col-sm-6 col-12">
+                                                    <label class="col-sm-12 col-form-label">المرجو إدخال محتوى الصورة <text style="color:red;">*</text></label>
+                                                    <div class="col-sm-12">
+                                                        <input name="CaptchaValue" autocomplete="off" class="form-control" data-val="true" data-val-required="خانة إجبارية" id="CaptchaValue" maxlength="5" onblur="ValidateInput(this, 12)" onkeyup="return GetMyKeyCode(this,6);" type="text" value="">
+                                                        <span name="CaptchaValue" class="field-validation-valid" data-valmsg-for="CaptchaValue" data-valmsg-replace="true"></span>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <br>
+                                            <div class="row">
+                                                <div class="form-group col-md-12 text-center">
+                                                    <button type="submit" id="btnSendDemand" class="btn btn-success" style="width:100px;">تأكيد <i id="imgLoaderbtnSendDemand" style="display:none" class="fa fa-spinner fa-spin fa-lg"> </i></button>
+                                                </div>
+                                            </div>
+                                        </fieldset>
+</form>                                    
+                                    <div id="accordion" class="Formulaire-SuiviDaam">
+                                        <div class="card">
+                                            <fieldset class="Formulaire">
+                                                <legend>وضعية الطلب</legend>
+                                                <div class="card-body">
+                                                    <div class="Formulaire Result">
+                                                        <h6><i class="fa fa-check-circle" aria-hidden="true"></i> <text id="statutDemande"></text></h6>
+                                                        
+                                                    </div>
+                                                </div>
+                                            </fieldset>
+                                        </div>
+                                        <div class="card" id="infoPayement">
+                                            <fieldset class="Formulaire">
+                                                <legend>معلومات حول الأداء</legend>
+                                                <div class="card-body">
+                                                    <div class="Formulaire Messages">
+                                                        <ul>
+                                                            <li id="periode1"><p class="msg" id="statutPremierePeriodeDemande"></p></li>
+                                                            <li id="periode2"><p class="msg" id="statutDeuxiemePeriodeDemande"></p></li>
+                                                        </ul>
+                                                    </div>
+                                                </div>
+                                            </fieldset>
+                                        </div>
+                                        <p id="reclamationExistsId" class="text-center" style="font-size:22px; color:black; margin-top:30px"></p>
+                                        <div class="card" id="reclamationDivId" style="display:none;">
+                                            <fieldset class="Formulaire">
+                                                <legend>استمارة الشكاية</legend>
+                                                <div class="card-body" id="divFormPlainteId">
+
+                                                </div>
+                                            </fieldset>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <br>
+            </div>
+        </div>
+    </div>
+</main>
+
+<footer id="main-footer" class="text-rtl text-light bg-dark">
+    <div class="container py-3">
+        <div class="row">
+            <div class="text-center col-12 col-md"> <small>© 2020</small> - <small> المملكة المغربية</small> </div>
+        </div>
+    </div>
+</footer>
+
+<!-- JavaScript
+    ================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script>
+    $(document).ready(function () {
+    });
+</script>
+    
+
+</body></html>
